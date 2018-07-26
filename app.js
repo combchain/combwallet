7ad23d094939e562de6d57d928d7dde7ed18c888
danforthpeter <danforthpeter66@gmail.com>
2018-07-26 17:10:45 +0800

    Transactions and alert page

--- app.js
+++ app.js
@@ -73,4 +73,205 @@ app.directive('onReadFile',
       }
     };
   });
+var Notifier = {
+  show: false,
+  class: "",
+  icon: "",
+  message: "",
+  timer: null,
+  sce: null,
+  scope: null,
+
+  open: function open() {
+    this.show = true;
+    if (!this.scope.$$phase) this.scope.$apply();
+  },
+
+  close: function close() {
+    this.show = false;
+    if (!this.scope.$$phase) this.scope.$apply();
+  },
+
+  warning: function warning(msg) {
+    this.class = "alert-warning";
+    this.icon = "fa fa-question-circle";
+    this.showAlert(this.class, msg);
+  },
+
+  info: function info(msg) {
+    this.class = "alert-info";
+    this.icon = "fa fa-info-circle";
+    this.showAlert(this.class, msg);
+    this.setTimer();
+  },
+
+  danger: function danger(msg) {
+    this.class = "alert-danger";
+    this.icon = "fa fa-times-circle";
+    this.showAlert(this.class, msg);
+  },
+
+  success: function success(msg) {
+    this.class = "alert-success";
+    this.icon = "fa fa-check-circle";
+    this.showAlert(this.class, msg);
+  },
+
+  showAlert: function showAlert(_class, msg) {
+    clearTimeout(this.timer);
+    this.class = _class;
+    this.message = this.sce.trustAsHtml(msg);
+    this.open();
+  },
+
+  setTimer: function setTimer() {
+    var _this = this;
+    clearTimeout(_this.timer);
+    _this.timer = setTimeout(function() {
+        _this.show = false;
+        if (!_this.scope.$$phase) _this.scope.$apply();
+      },
+      5000);
+  }
+};
+var Notifier = {
+  show: false,
+  class: "",
+  icon: "",
+  message: "",
+  timer: null,
+  sce: null,
+  scope: null,
+
+  open: function open() {
+    this.show = true;
+    if (!this.scope.$$phase) this.scope.$apply();
+  },
+
+  close: function close() {
+    this.show = false;
+    if (!this.scope.$$phase) this.scope.$apply();
+  },
+
+  warning: function warning(msg) {
+    this.class = "alert-warning";
+    this.icon = "fa fa-question-circle";
+    this.showAlert(this.class, msg);
+  },
+
+  info: function info(msg) {
+    this.class = "alert-info";
+    this.icon = "fa fa-info-circle";
+    this.showAlert(this.class, msg);
+    this.setTimer();
+  },
+
+  danger: function danger(msg) {
+    this.class = "alert-danger";
+    this.icon = "fa fa-times-circle";
+    this.showAlert(this.class, msg);
+  },
+
+  success: function success(msg) {
+    this.class = "alert-success";
+    this.icon = "fa fa-check-circle";
+    this.showAlert(this.class, msg);
+  },
+
+  showAlert: function showAlert(_class, msg) {
+    clearTimeout(this.timer);
+    this.class = _class;
+    this.message = this.sce.trustAsHtml(msg);
+    this.open();
+  },
+
+  setTimer: function setTimer() {
+    var _this = this;
+    clearTimeout(_this.timer);
+    _this.timer = setTimeout(function() {
+        _this.show = false;
+        if (!_this.scope.$$phase) _this.scope.$apply();
+      },
+      5000);
+  }
+};
+app.controller('ModalInstanceCtrl',
+  function($scope, $modalInstance, items) {
+    $scope.txModify = false;
+
+    if ($scope.txType == '128') {
+      $scope.FromAddress = Wallet.toAddress(hexstring2ab(items.fromAddress));
+      $scope.ToAddress = Wallet.toAddress(items.tx.outputs[0].scripthash);
+
+      var valueStr = ab2hexstring(reverseArray(items.tx.outputs[0].value));
+
+      $scope.Value = WalletMath.div(WalletMath.hexToNumToStr(valueStr), 100000000);
+      $scope.ValueView = WalletMath.fixView($scope.Value);
+      $scope.AssetIDRev = ab2hexstring(reverseArray(items.tx.outputs[0].assetid));
+      $scope.AssetID = ab2hexstring(items.tx.outputs[0].assetid);
+      $scope.AssetName = "NULL";
+
+      for (i = 0; i < $scope.coins.length; i++) {
+        if ($scope.coins[i].AssetId == $scope.AssetIDRev) {
+          $scope.AssetName = $scope.coins[i].AssetName;
+        }
+      }
+
+
+      if (items.toAddress != $scope.ToAddress) {
+        console.log("ToAddress verify failed.");
+        $scope.txModify = true;
+      }
+
+
+      if (!WalletMath.eq(items.amount, $scope.Value)) {
+        console.log("Amount verify failed.");
+        $scope.txModify = true;
+      }
+
+
+      if (items.tx.outputs.length == 2) {
+        if (Wallet.toAddress(items.tx.outputs[1].scripthash) != $scope.FromAddress) {
+          console.log("FromAddress verify failed.");
+          $scope.txModify = true;
+        }
+      }
+    } else if ($scope.txType == '2') {
+      $scope.ClaimAddress = Wallet.toAddress(hexstring2ab(items.claimAddress));
+
+      var valueStr = ab2hexstring(reverseArray(items.tx.outputs[0].value));
+      $scope.Value = parseInt(valueStr, 16);
+      $scope.AssetID = ab2hexstring(reverseArray(items.tx.outputs[0].assetid));
+      $scope.AssetName = "Â∞èËöÅÂ∏Å";
+
+
+      if (items.amount != $scope.Value) {
+        console.log("Amount verify failed.");
+        $scope.txModify = true;
+      }
+
+
+      if (Wallet.toAddress(items.tx.outputs[0].scripthash) != $scope.ClaimAddress) {
+        console.log("ClaimAddress verify failed.");
+        $scope.txModify = true;
+      }
+    }
+
+
+    $scope.ok = function() {
+      if (!$scope.txModify) {
+        if ($scope.walletType == 'externalsignature') {
+          $scope.MakeTxAndSend(items.txData);
+        } else {
+          $scope.SignTxAndSend(items.txData);
+        }
+      }
+      $modalInstance.close();
+    };
+
+
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel');
+    }
+  });
 
